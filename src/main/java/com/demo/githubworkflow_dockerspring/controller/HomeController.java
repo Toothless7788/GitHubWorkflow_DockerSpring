f489a3036a1089cf86b2f796a06de5c977ed110b
w45242hy <hin.yip-2@student.manchester.ac.uk>
2025-04-10 20:19:17 +0100

    feat: update HomeController message

--- src/main/java/com/demo/githubworkflow_dockerspring/controller/HomeController.java
+++ src/main/java/com/demo/githubworkflow_dockerspring/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	@GetMapping
 	public String index() {
-		return "This is an example project of using GitHub Workflow for CI/CD";
+		return "This is an example project which demonstrates the CI/CD using GitHub Workflow";
 	}
 }
